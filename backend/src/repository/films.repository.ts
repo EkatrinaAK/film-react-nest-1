0145bee4b698ebec9a396fe52b47407fb15855e6
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-05-26 15:02:14 +0300

    Order controller and service

--- backend/src/repository/films.repository.ts
+++ backend/src/repository/films.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { GetFilmDTO, GetScheduleDTO } from '../films/dto/films.dto';
@@ -42,7 +42,7 @@ export class FilmsRepository {
   }
 
   async findAllFilms(): Promise<{ total: number; items: GetFilmDTO[] }> {
-    const items = await this.filmModel.find({}); //используем обычные методы Mongoose-документов
+    const items = await this.filmModel.find({}); 
     const total = await this.filmModel.countDocuments({});
     return {
       total,
@@ -50,14 +50,73 @@ export class FilmsRepository {
     };
   }
 
-  async findScheduleById(
+  async findAllSchedulesById(
     filmId: string,
   ): Promise<{ total: number; items: GetScheduleDTO[] }> {
-    const film = await this.filmModel.findOne({ id: filmId }); //используем обычные методы Mongoose-документов
+    const film = await this.findFilmById(filmId); 
     const schedule = film.schedule;
     return {
       total: schedule.length,
       items: schedule.map(this.getScheluderFilm()),
     };
+  }
+
+  async findFilmById(filmId: string): Promise<GetFilmDTO> {
+    try {
+      const film = await this.filmModel.findOne({ id: filmId });
+      const mapper = this.getFilm();
+      return mapper(film);
+    } catch {
+      throw new NotFoundException(`Фильм с ${filmId} не найден`);
+    }
+  }
+
+  async findSchedulesById(
+    filmId: string,
+    scheduleId: string,
+  ): Promise<GetScheduleDTO> {
+    const { items } = await this.findAllSchedulesById(filmId);
+    const schedule = items.find((el) => el.id === scheduleId);
+    if (!schedule) {
+      throw new NotFoundException(`Сеанса с ${scheduleId} не найден`);
+    }
+    return schedule;
+  }
+
+  async checkPlace(
+    filmId: string,
+    scheduleId: string,
+    place: string,
+  ): Promise<boolean> {
+    const res = await this.filmModel.find({
+      id: filmId,
+      schedule: {
+        $elemMatch: {
+          id: scheduleId,
+          taken: place,
+        },
+      },
+    });
+    return Boolean(res.length);
+  }
+
+  async updatePlaces(
+    filmId: string,
+    scheduleId: string,
+    place: string,
+  ): Promise<void> {
+    await this.filmModel.updateOne(
+      {
+        id: filmId,
+        schedule: {
+          $elemMatch: {
+            id: scheduleId,
+          },
+        },
+      },
+      {
+        $push: { 'schedule.$.taken': place },
+      },
+    );
   }
 }
