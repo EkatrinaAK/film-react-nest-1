079b1546f1c7797d3d0e17ae3f84d49638762949
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-05-27 21:15:12 +0300

    на проверку

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
-
 import { configProvider } from './app.config.provider';
 import { FilmsController } from './films/films.controller';
 import { OrderController } from './order/order.controller';
