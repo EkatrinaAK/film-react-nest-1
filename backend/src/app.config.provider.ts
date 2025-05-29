2aae80ed76c440fb5e0224f21496765339e93ad9
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-05-29 16:55:12 +0300

    change env, schedule.entity, app.config, feat repo mongo and postgres

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -7,8 +7,13 @@ export const configProvider = {
   useValue: <AppConfig>{
     //TODO прочесть переменнные среды
     database: {
-      driver: applicationConfig.DATABASE_DRIVER,
-      url: applicationConfig.DATABASE_URL,
+      driver: applicationConfig.DATABASE_DRIVER || 'postgres',
+      url: applicationConfig.DATABASE_URL || 'postgresql://localhost',
+      host: applicationConfig.DATABASE_HOST || 'postgres',
+      port: applicationConfig.DATABASE_PORT || 5432,
+      username: applicationConfig.DATABASE_USERNAME || 'prac',
+      password: applicationConfig.DATABASE_PASSWORD || 'prac',
+      database: applicationConfig.DATABASE_NAME || 'prac',
     },
   },
 };
@@ -20,4 +25,9 @@ export interface AppConfig {
 export interface AppConfigDatabase {
   driver: string;
   url: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
 }
